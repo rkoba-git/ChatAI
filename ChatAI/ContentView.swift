4cbfd42e99ce844be487e41e390710c511de79c6
rkoba-git <rinrin1311@icloud.com>
2023-01-07 23:40:13 +0900

    SubView(AIView)の作成

--- ChatAI/ContentView.swift
+++ ChatAI/ContentView.swift
@@ -2,56 +2,13 @@
 import SwiftUI
 
 struct ContentView: View{
-    @State private var text = "" //@Stateを使用することで，その変数が変わるたびにpreview側も変化する
-    @State private var answer = ""
     
     @StateObject private var appHideControl = AppHideControl.shared
     
     var body: some View {
         ZStack {
-            VStack {
-                Text("Question to AI")
-                    .font(.largeTitle)
-                    .fontWeight(.black)
-                    .multilineTextAlignment(.center)
-                    .padding()
-                
-                TextField("質問", text: $text,
-                          onCommit: {
-                    print("打ち込み終了")
-                    AppHideControl.shared.showLoading()
-                    APICaller.shared.getResponse(input: text) { result in
-                        switch result{
-                        case .success(let output):
-                            DispatchQueue.main.async {
-                                AppHideControl.shared.hideLoading()
-                                            }
-                            answer = output
-                            print("解答の取得に成功")
-                        default:
-                            DispatchQueue.main.async {
-                                AppHideControl.shared.hideLoading()
-                                            }
-                            print("AIの解答の取得に失敗")
-                        }
-                    }
-                })
-                .textFieldStyle(RoundedBorderTextFieldStyle())
-                .padding()
-                // ドル記号($)は，状態変数(@Stateを付与した変数)の参照を渡す役割
-                //参照したい状態変数名の前に$をつけることで使用することができる
-                Text("AI's answer")
-                    .fontWeight(.black)
-                    .multilineTextAlignment(.center)
-                Text(answer)
-                    .fontWeight(.black)
-                    .multilineTextAlignment(.center)
-                Spacer()
-                Image("AIimage") //svgファイルなら背景透過可能
-                    .resizable()
-                    .scaledToFit()
-                    .frame(width: 300.0, height: 120.0)
-            }.background(Color(hue: 0.512, saturation: 0.565, brightness: 0.884))
+            //.ignoresSafeArea(.keyboard, edges: .bottom)によってキーボードによる隠れを回避
+            AIView().ignoresSafeArea(.keyboard, edges: .bottom)
             LoadingView().zIndex(appHideControl.zIndex)
         }
     }
@@ -72,6 +29,62 @@ struct LoadingView: View {
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        Group {
+            ContentView()
+                .previewInterfaceOrientation(.portrait)
+        }
+    }
+}
+
+struct AIView: View {
+    //@Stateを使用することで，その変数が変わるたびにpreview側も変化する
+    @State private var text = ""
+    @State private var answer = ""
+       
+    
+    var body: some View {
+        VStack {
+            Text("Question to AI")
+                .font(.largeTitle)
+                .fontWeight(.black)
+                .multilineTextAlignment(.center)
+                .padding()
+            
+            TextField("質問", text: $text,
+                      onCommit: {
+                print("打ち込み終了")
+                AppHideControl.shared.showLoading()
+                APICaller.shared.getResponse(input: text) { result in
+                    switch result{
+                    case .success(let output):
+                        DispatchQueue.main.async {
+                            AppHideControl.shared.hideLoading()
+                        }
+                        answer = output
+                        print("解答の取得に成功")
+                    default:
+                        DispatchQueue.main.async {
+                            AppHideControl.shared.hideLoading()
+                        }
+                        print("AIの解答の取得に失敗")
+                    }
+                }
+            })
+            .textFieldStyle(RoundedBorderTextFieldStyle())
+            .padding()
+            // ドル記号($)は，状態変数(@Stateを付与した変数)の参照を渡す役割
+            //参照したい状態変数名の前に$をつけることで使用することができる
+            Text("AI's answer")
+                .fontWeight(.black)
+                .multilineTextAlignment(.center)
+            Text(answer)
+                .fontWeight(.black)
+                .multilineTextAlignment(.center)
+            Spacer()
+            Image("AIimage") //svgファイルなら背景透過可能
+                .resizable()
+                .scaledToFit()
+                .frame(width: 300.0, height: 120.0)
+        }.background(Color(hue: 0.512, saturation: 0.565, brightness: 0.884))
     }
 }
